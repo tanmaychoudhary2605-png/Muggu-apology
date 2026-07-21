5a6917871dc57798e94ba6b572922b937c76fff8
tanmaychoudhary2605-png <tanmaychoudhary2605@gmail.com>
2026-07-21 10:28:26 +0530

    Add functions for journey navigation and popup

---
+++ script.js
@@ -0,0 +1,15 @@
+function startJourney() {
+  document.querySelector(".hero").style.display = "none";
+  document.getElementById("story").style.display = "flex";
+}
+function nextPage() {
+  story.style.display = "none";
+  gallery.style.display = "flex";
+}
+function nextFinal() {
+  gallery.style.display = "none";
+  document.querySelector(".final-section").style.display = "flex";
+}
+function forgiveMe() {
+  popup.style.display = "block";
+}
